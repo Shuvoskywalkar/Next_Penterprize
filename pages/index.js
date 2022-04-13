803b3c4960b3226fa20c6c71f8c980d2727a4ca2
Shuvoskywalkar <shuvokoiri89@gmail.com>
2022-04-13 17:44:29 +0600

    second commi2t

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
-import Homepage from "../Components/Homepage/Homepage";
-import { Fragment } from 'react';
-export default function Home(props) {
+export default function Home() {
   return (
-    <Fragment  >
-    
-    <Homepage products={props.meetupPlaces} />
-
-  </Fragment>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-export async function getStaticProps(){
-  const res = await fetch('https://fakestoreapi.com/products')
-  const posts = await res.json()
-
- return{
-   props:{
-    meetupPlaces:posts.map((dta)=>({
-      image:dta.image,
-      description:dta.description,
-      address:dta.category,
-      _id:dta.id,
-      title:dta.title,
-      price:dta.price
-    })),
-  },
-  revalidate:10,
-};
-} 
